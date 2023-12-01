dea4c1a6e6cc9e70cfe2d4aa1ab8b405a839ec32
Dzhura Rostislav <dzhurarostislav@gmail.com>
2023-12-01 18:21:49 +0200

    Add initial migration file to project

--- kitchen/models.py
+++ kitchen/models.py
@@ -26,7 +26,7 @@ class Cook(AbstractUser):
 class Dish(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
     ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
     cooks = models.ManyToManyField(Cook, related_name="dishes")
