271b9e879da39ed56e717daecde8e27e572db32e
Dzhura Rostislav <dzhurarostislav@gmail.com>
2023-12-01 18:19:10 +0200

    Add models to models.py

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class IngredientType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    type = models.ForeignKey(IngredientType, on_delete=models.CASCADE, related_name="ingredients")
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField()
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
