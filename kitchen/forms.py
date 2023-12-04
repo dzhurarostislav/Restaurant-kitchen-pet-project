ebc40af1cd1d2fdc02bf7b71b487b98cd8e0eee0
Dzhura Rostislav <dzhurarostislav@gmail.com>
2023-12-04 23:06:15 +0200

    Add custom middleware to redirect authenticated users from login page and begin to create navbar

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -6,8 +6,17 @@ from kitchen.models import Cook
 
 
 class CookCreationForm(UserCreationForm):
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.fields['password2'].help_text = None
+        self.fields['years_of_experience'].help_text = \
+            "Number of years worked in the kitchen"
+
     class Meta:
         model = Cook
         fields = UserCreationForm.Meta.fields + (
-
+            "first_name",
+            "last_name",
+            "years_of_experience",
         )
--- restaurant_kitchen/settings.py
+++ restaurant_kitchen/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'kitchen.middleware.RedirectAuthenticatedUsersMiddleware'
 ]
 
 ROOT_URLCONF = 'restaurant_kitchen.urls'
