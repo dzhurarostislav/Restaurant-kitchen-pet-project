3974490fc8e40725460140d6a9589c55b719e936
Dzhura Rostislav <dzhurarostislav@gmail.com>
2023-12-12 03:53:37 +0200

    add restaurant-kitchen-0cd1.onrender.com to ALLOWED_HOSTS

--- restaurant_kitchen/settings.py
+++ restaurant_kitchen/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "restaurant-kitchen-0cd1.onrender.com"]
 
 # Application definition
 
